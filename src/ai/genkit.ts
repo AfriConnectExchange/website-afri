7e3483a62815ca0f851f2717bd0ec4845ebee460
AFRI CONNECT <africonnectorg@gmail.com>
2025-10-19 21:48:09 +0000

    ## Error Type Build Error
    ## Error Message
    Module not found: Can't reso

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,23 +1,2 @@
-'use server';
-import {genkit, configureGenkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
-import {firebase} from '@genkit-ai/firebase';
-import {genkitEval, GenkitMetric} from '@genkit-ai/evaluator';
-import {dotprompt} from '@genkit-ai/dotprompt';
 
-configureGenkit({
-  plugins: [
-    firebase(),
-    googleAI(),
-    genkitEval({
-      judge: 'googleai/gemini-pro',
-      metrics: [GenkitMetric.Faithfulness, GenkitMetric.AnswerRelevancy],
-      embedder: 'googleai/text-embedding-004',
-    }),
-    dotprompt(),
-  ],
-  logLevel: 'debug',
-  enableTracingAndMetrics: true,
-});
 
-export const ai = genkit;
